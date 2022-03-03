bbf3d9779f265fc271cc0c0afaa6e22841d56e81
shruthi093 <jayasruthi.l@gmail.com>
2022-03-03 20:31:25 +0530

    find

---
+++ routes/getUserbyArea.js
@@ -0,0 +1,16 @@
+const express=require("express")
+const router=express.Router()
+const bcrypt = require("bcrypt")
+const User=require("../models/UserModel")
+router.get("/getbyarea/",async (req,res)=>{
+const querydata=req.query
+const areadata=await User.find({area:querydata.area})
+console.log(areadata)
+if(areadata.length>0){
+    res.send(areadata)
+}
+else{
+    res.send({status:false,msg:"no results found"})
+}
+})
+module.exports=router
